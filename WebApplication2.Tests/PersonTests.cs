89513e5adec42f967393d24f1cbe23525ed15289
ballen <ballen@JSMDQ32>
2018-05-16 15:46:12 -0400

    using moq for unit tests

---
+++ WebApplication2.Tests/PersonTests.cs
@@ -0,0 +1,27 @@
+﻿namespace WebApplication2.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using BusinessLayer.Interfaces;
+    using DataLayerInterfaces;
+    using Moq;
+
+    [TestClass]
+    public class PersonTests
+    {
+        private IPersonBusinessLayer personBusinessLayer;
+        private Mock<IPersonDataLayer> moq_personDataLayer;
+
+        public void Initialize()
+        {
+            moq_personDataLayer = new Mock<IPersonDataLayer>();
+
+            moq_personDataLayer.Setup(dl => dl.GetTextData())
+                .Returns(new )
+        }
+    }
+}
